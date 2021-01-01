ba8de225a3b41db10dd7731acfa4465987361485
Vaishnavi Nilkanth Muley <54570348+VaishnaviMuley19@users.noreply.github.com>
2021-01-01 22:52:46 +0530

    Add files via upload

---
+++ Google IT Automation with Python/Crash Course on Python/Assignment_3/Prog_1.py
@@ -0,0 +1,13 @@
+#The function receives a name,
+#then returns a greeting based on whether or not that name is "Taylor".
+def greeting(name):
+  if name == "Taylor":
+    return "Welcome back Taylor!"
+  else:
+    return "Hello there, " + name
+
+#print(greeting("Taylor"))
+#print(greeting("John"))
+username=input("Enter Your Name: ")
+result=greeting(username)
+print(result)
