aac4d3cc9d33858527be08dccf0e65ff91489d33
Vaishnavi Nilkanth Muley <54570348+VaishnaviMuley19@users.noreply.github.com>
2021-01-01 22:49:03 +0530

    Add files via upload

---
+++ Google IT Automation with Python/Crash Course on Python/Assignment_1/Prog_2.py
@@ -0,0 +1,6 @@
+#This code is supposed to take two numbers, divide one by another so that the result is equal to 1,
+#and display the result on the screen.
+numerator = 10
+denominator = 10
+result = numerator // denominator
+print(result)
---
+++ Google IT Automation with Python/Crash Course on Python/Assignment_1/Prog_4.py
@@ -0,0 +1,3 @@
+#This code is supposed to display "2 + 2 = 4" on the screen
+sum=4
+print("2 + 2 = " + str(sum))
