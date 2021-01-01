6831e033672a32d97af84921b40ddfea2cc29d77
Vaishnavi Nilkanth Muley <54570348+VaishnaviMuley19@users.noreply.github.com>
2021-01-01 22:56:33 +0530

    Add files via upload

---
+++ Google IT Automation with Python/Crash Course on Python/Assignment_6/Prog_3.py
@@ -0,0 +1,11 @@
+#Implement the sum_positive_numbers function, as a recursive function that returns
+#the sum of all positive numbers between the number n received and 1.
+#For example, when n is 3 it should return 1+2+3=6, and when n is 5 it should return 1+2+3+4+5=15.
+def sum_positive_numbers(n):
+  sum=0
+  if(n>0):
+    sum=n+sum_positive_numbers(n-1)
+  return sum
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
